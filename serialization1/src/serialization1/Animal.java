82ac437ffeedcecfc219842c92e2b2245e0a2100
yaminibalaga <yaminibalaga@LAPTOP-QREIKKIM>
2021-04-07 16:40:49 +0530

    serialization

---
+++ serialization1/src/serialization1/Animal.java
@@ -0,0 +1,8 @@
+package serialization1;
+
+public class Animal {
+	int i = 10;
+	public Animal() {
+		System.out.println("animals no arg constructor called");
+	}
+}
---
+++ serialization1/src/serialization1/Rat.java
@@ -0,0 +1,7 @@
+package serialization1;
+import java.io.Serializable;
+public class Rat implements Serializable{
+
+	int l=40;
+
+}
